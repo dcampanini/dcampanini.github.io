38ca9a1fff49dc674187384eb4eba3e97ba1c05b
dcampanini <campanini.diego@gmail.com>
2025-09-18 21:20:44 +0000

    Deploying to gh-pages from @ dcampanini/dcampanini.github.io@6583a7da93387f1dc0b81943c1342fa8e2584f05 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -504,6 +504,13 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=8PDuRXIAAAAJ&hl", "_blank");
         },
       },{
+        id: 'social-custom_social',
+        title: 'Custom_social',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://huggingface.co/dcampanini", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
