ad8a7aa86cef52050420e673e4828bb347627c8a
dcampanini <campanini.diego@gmail.com>
2025-09-19 16:58:23 +0000

    Deploying to gh-pages from @ dcampanini/dcampanini.github.io@c627d296dad838c5fb58fbe6cdd5c6eb02487376 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,16 +398,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-attended-acl-2024-to-present-2-works-ihealth-chile-1-at-rrg24-in-context-learning-and-finetuning-of-a-large-multimodal-model-for-radiology-report-generation-and-ihealth-chile-3-amp-amp-2-at-rrg24-template-based-report-generation",
+          title: 'I attended ACL 2024 to present 2 works iHealth-Chile-1 at RRG24: In-context Learning...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
